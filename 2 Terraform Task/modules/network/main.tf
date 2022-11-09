7e517b8661e20ad4edb096ec71749bab0134105d
Angelo Machils <angelo@machils.org>
2022-11-09 19:43:00 +0200

    Work on the Terraform task

---
+++ 2 Terraform Task/modules/network/main.tf
@@ -0,0 +1,10 @@
+# Creating Public Subnet
+resource "aws_subnet" "publicsubnet" {
+  vpc_id = "${aws_vpc.am_vpc}"
+  cidr_block = var.subnet_cidr[0]
+  availability_zone = "eu-north-1"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = var.publicsubnet_name[0]
+  }
+}
--- 2 Terraform Task/modules/network/vars.tf
+++ 2 Terraform Task/modules/network/vars.tf
@@ -4,3 +4,10 @@ variable "subnet_cidr" {
   type = list
   default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
+
+# Define the name of the public subnet
+variable "publicsubnet_name" {
+  description = "Define the names of the subnets"
+  type = list
+  default = ["Public Subnet", "Private Subnet"]
+}
