9ae8a179678a519e59252624d6729fce17d5d615
Angelo Machils <angelo@machils.org>
2022-11-13 20:22:56 +0200

    Some fine-tuning on existing code.

--- 2 Terraform Task/modules/network/main.tf
+++ 2 Terraform Task/modules/network/main.tf
@@ -1,31 +1,31 @@
-# Creating Public Subnet
+# Create Public Subnet
 resource "aws_subnet" "publicsubnet" {
-  vpc_id = var.vpc_id
-  cidr_block = var.subnet_cidr[0]
-  availability_zone = "eu-north-1a"
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_cidr[0]
+  availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = var.subnet_name[0]
   }
 }
 
-# Creating Private Subnet
+# Create Private Subnet
 resource "aws_subnet" "privatesubnet" {
-  vpc_id = var.vpc_id
-  cidr_block = var.subnet_cidr[1]
-  availability_zone = "eu-north-1a"
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_cidr[1]
+  availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = false
   tags = {
     Name = var.subnet_name[1]
   }
 }
 
-# Creating Internet Gateway
+# Create Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = var.vpc_id
 }
 
-# Creating Route Table for Public Subnet
+# Create Route Table for Public Subnet
 resource "aws_route_table" "rt" {
   vpc_id = var.vpc_id
   route {
@@ -37,6 +37,6 @@ resource "aws_route_table" "rt" {
   }
 }
 resource "aws_route_table_association" "rt_associate_public" {
-  subnet_id = aws_subnet.publicsubnet.id
+  subnet_id      = aws_subnet.publicsubnet.id
   route_table_id = aws_route_table.rt.id
 }
--- 2 Terraform Task/modules/network/vars.tf
+++ 2 Terraform Task/modules/network/vars.tf
@@ -1,12 +1,12 @@
-# Defining CIDR Block for subnet
+# Define CIDR Block for subnet
 variable "subnet_cidr" {
-  type = list
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  type    = list(any)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 # Define the name of the subnets
 variable "subnet_name" {
-  type = list
+  type    = list(any)
   default = ["Public Subnet", "Private Subnet"]
 }
 
@@ -14,8 +14,8 @@ variable "vpc_id" {
   type = string
 }
 
-# Defining the name of the route tables
+# Define the name of the route tables
 variable "route_name" {
-  type = list
+  type    = list(any)
   default = ["Public Route", "Private Route"]
 }
