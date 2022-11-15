e43b92d867f5bdd8c3963d7d7b44c6bdd14c2733
Angelo Machils <angelo@machils.org>
2022-11-15 16:17:38 +0200

    Start EC2 & ELB Task.

--- 3 EC2 ELB Task/providers.tf
+++ 3 EC2 ELB Task/providers.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  # backend "s3" {
-  #   encrypt        = true
-  #   bucket         = "tfams3bucket"
-  #   dynamodb_table = "terraform-state-lock-dynamo-tfams3bucket"
-  #   key            = "terraform.tfstate"
-  #   region         = "eu-north-1"
-  # }
+  backend "s3" {
+    encrypt        = true
+    bucket         = "tfams3bucket"
+    dynamodb_table = "terraform-state-lock-dynamo-tfams3bucket"
+    key            = "terraform.tfstate"
+    region         = "eu-north-1"
+  }
 
   required_providers {
     aws = {
