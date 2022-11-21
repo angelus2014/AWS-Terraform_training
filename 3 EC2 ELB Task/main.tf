9760d7f58c11125d27df85c646ac43d5aa2b5f8a
Angelo Machils <angelo@machils.org>
2022-11-21 13:46:44 +0200

    Working on the load balancer.

--- 3 EC2 ELB Task/main.tf
+++ 3 EC2 ELB Task/main.tf
@@ -10,36 +10,16 @@ module "network" {
   vpc_id = module.vpc.vpc_id
 }
 
-# Create the ec2 components: EC2, Routing Table
-module "ec2" {
-  source = "./modules/ec2"
+# Create the load balancer components: EC2, Routing Table, auto scaling
+module "elb" {
+  source = "./modules/elb"
 
   vpc_id     = module.vpc.vpc_id
   subnet_id1 = module.network.subnet_id1
   subnet_id2 = module.network.subnet_id2
 }
 
-# # Create the S3 bucket resources
-# resource "aws_s3_bucket" "tf_am_s3" {
-#   bucket = var.s3_bucket_name
-#   tags = {
-#     Name = var.s3_friendly_name
-#   }
-# }
-
-# resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-#   bucket = aws_s3_bucket.tf_am_s3.id
-#   acl    = "private"
-# }
-
-# resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-#   name           = "terraform-state-lock-dynamo-tfams3bucket"
-#   hash_key       = "LockID"
-#   read_capacity  = 20
-#   write_capacity = 20
-
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+# Create the S3 components
+module "s3" {
+  source = "./modules/s3"
+}
