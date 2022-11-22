426259364b31f19ae21a84834a89ea1bae3634ee
Angelo Machils <angelo@machils.org>
2022-11-22 16:46:21 +0200

    Still struggling with the load balancer

--- 3 EC2 ELB Task/main.tf
+++ 3 EC2 ELB Task/main.tf
@@ -18,7 +18,10 @@ locals {
 
   user_data = <<-EOT
   #!/bin/bash
-  echo "Hello World!"
+yum update -y
+amazon-linux-extras install -y nginx1
+systemctl start nginx
+systemctl enable nginx
   EOT
 }
 
@@ -64,9 +67,9 @@ module "auto-scaling-group-demo" {
 
   vpc_zone_identifier = module.vpc.private_subnets
   security_groups     = [module.asg_sg.security_group_id]
-  min_size            = 0
-  max_size            = 1
-  desired_capacity    = 1
+  min_size            = 1
+  max_size            = 3
+  desired_capacity    = 2
 
   create_launch_template = false
   launch_template        = aws_launch_template.this.name
@@ -106,4 +109,184 @@ module "asg_sg" {
   egress_rules = ["all-all"]
 
   tags = local.tags
+}
+
+
+# module "network_load_balancer" {
+#   source  = "infrablocks/network-load-balancer/aws"
+#   version = "~> 0.2.0"
+
+#   region     = local.region
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = public_subnets
+
+#   # component             = "important-component"
+#   # deployment_identifier = "production"
+
+#   # domain_name     = "example.com"
+#   # public_zone_id  = "Z1WA3EVJBXSQ2V"
+#   # private_zone_id = "Z3CVA9QD5NHSW3"
+
+#   listeners = [
+#     {
+#       lb_port            = 443
+#       lb_protocol        = "HTTPS"
+#       instance_port      = 443
+#       instance_protocol  = "HTTPS"
+#       ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/default"
+#     },
+#     {
+#       lb_port           = 80
+#       lb_protocol       = "HTTP"
+#       instance_port     = 80
+#       instance_protocol = "HTTP"
+#     },
+#     {
+#       lb_port           = 6567
+#       lb_protocol       = "TCP"
+#       instance_port     = 6567
+#       instance_protocol = "TCP"
+#     }
+#   ]
+
+#   access_control = [
+#     {
+#       lb_port       = 443
+#       instance_port = 443
+#       allow_cidr    = "0.0.0.0/0"
+#     },
+#     {
+#       lb_port       = 80
+#       instance_port = 80
+#       allow_cidr    = "0.0.0.0/0"
+#     },
+#     {
+#       lb_port       = 6567
+#       instance_port = 6567
+#       allow_cidr    = "0.0.0.0/0"
+#     }
+#   ]
+
+#   egress_cidrs = "0.0.0.0/0"
+
+#   health_check_target              = "HTTPS:443/ping"
+#   health_check_timeout             = 10
+#   health_check_interval            = 30
+#   health_check_unhealthy_threshold = 5
+#   health_check_healthy_threshold   = 5
+
+#   enable_cross_zone_load_balancing = "yes"
+
+#   enable_connection_draining  = "yes"
+#   connection_draining_timeout = 60
+
+#   idle_timeout = 60
+
+#   include_public_dns_record  = "yes"
+#   include_private_dns_record = "yes"
+
+#   expose_to_public_internet = "yes"
+# }
+
+# module "alb" {
+#   source  = "terraform-aws-modules/alb/aws"
+#   version = "~> 8.0"
+
+#   name = "my-alb"
+
+#   load_balancer_type = "application"
+
+#   vpc_id             = module.vpc.vpc_id
+#   subnets            = public_subnets
+#   security_groups    = ["sg-edcd9784", "sg-edcd9785"]
+
+#   # access_logs = {
+#   #   bucket = "my-alb-logs"
+#   # }
+
+#   target_groups = [
+#     {
+#       name_prefix      = "pref-"
+#       backend_protocol = "HTTP"
+#       backend_port     = 80
+#       target_type      = "instance"
+#       targets = {
+#         my_target = {
+#           target_id = "i-0123456789abcdefg"
+#           port = 80
+#         }
+#         my_other_target = {
+#           target_id = "i-a1b2c3d4e5f6g7h8i"
+#           port = 8080
+#         }
+#       }
+#     }
+#   ]
+
+#   https_listeners = [
+#     {
+#       port               = 443
+#       protocol           = "HTTPS"
+#       certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+#       target_group_index = 0
+#     }
+#   ]
+
+#   http_tcp_listeners = [
+#     {
+#       port               = 80
+#       protocol           = "HTTP"
+#       target_group_index = 0
+#     }
+#   ]
+
+#   tags = {
+#     Environment = "Test"
+#   }
+# }
+
+module "nlb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "my-nlb"
+
+  load_balancer_type = "network"
+
+  vpc_id  = module.vpc.vpc_id
+  subnets = ["subnet-0d239f67e8c015018", "subnet-0b170a4507d95b19c", "subnet-0d0a24c09ae1f9e48"]
+
+  # access_logs = {
+  #   bucket = "my-nlb-logs"
+  # }
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "TCP"
+      backend_port     = 80
+      target_type      = "ip"
+    }
+  ]
+
+  https_listeners = [
+    {
+      port               = 443
+      protocol           = "TLS"
+      certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+      target_group_index = 0
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "TCP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Test"
+  }
 }
