574b11caf9f1332249b219691de90f82af9aa1a1
Angelo Machils <angelo@machils.org>
2022-11-28 16:03:23 +0200

    At last, a working Auto Scaling Group with template and created EC2 instance

--- 3 EC2 ELB Task/modules/lb/main.tf
+++ 3 EC2 ELB Task/modules/lb/main.tf
@@ -1,43 +1,3 @@
-# # Creating Security Group
-# resource "aws_security_group" "sg" {
-#   tags = {
-#     Name = var.sg_name
-#   }
-#   vpc_id = var.vpc_id
-#   # key_name = var.key_name
-
-#   # Inbound Rules
-#   # HTTP access from anywhere
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   # HTTPS access from anywhere
-#   ingress {
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   # SSH access from anywhere
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   # Outbound Rules
-#   # Internet access to anywhere
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
 resource "aws_key_pair" "key_pair" {
   key_name   = var.key_name
   public_key = file("${abspath(path.cwd)}/my-key.pub")
@@ -81,10 +41,12 @@ resource "aws_launch_template" "this" {
   instance_type = "t3.micro"
 
   network_interfaces {
-    associate_public_ip_address = true
+    associate_public_ip_address = false
     delete_on_termination       = true
+    # subnet_id                   = var.private_subnets
+    # security_groups             = [module.asg_sg.security_group_id]
   }
-
+  key_name = var.key_name
   lifecycle {
     create_before_destroy = true
   }
@@ -96,7 +58,7 @@ module "auto-scaling-group-demo" {
 
   name = "external-${local.name}"
 
-  vpc_zone_identifier = module.vpc.public_subnets
+  vpc_zone_identifier = module.vpc.private_subnets
   security_groups     = [module.asg_sg.security_group_id]
   min_size            = 0
   max_size            = 1
@@ -130,12 +92,12 @@ module "asg_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
-  name        = local.name
-  description = "A security group"
-  # vpc_id      = var.
-  vpc_id        = module.vpc.vpc_id
-  ingress_rules = ["all-all"]
-  egress_rules  = ["all-all"]
+  name                = local.name
+  description         = "A security group"
+  vpc_id              = module.vpc.vpc_id
+  ingress_cidr_blocks = ["10.99.0.0/18"]
+  ingress_rules       = ["http-80-tcp"]
+  egress_rules        = ["all-all"]
 
   tags = local.tags
 }
--- 3 EC2 ELB Task/modules/network/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Create a pubblicsubnet.id variable
-output "subnet_id1" {
-  value = aws_subnet.publicsubnet.id
-}
-
-# Create a privatesubnet.id variable
-output "subnet_id2" {
-  value = aws_subnet.privatesubnet.id
-}
