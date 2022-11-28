abf77e9abe0ecc04527c320c7878aad90be12278
Angelo Machils <angelo@machils.org>
2022-11-28 16:35:59 +0200

    Some cleanup

--- 3 EC2 ELB Task [backup]/modules/vpc/main.tf
+++
@@ -1,8 +0,0 @@
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-
-  tags = {
-    Name = var.vpc_name
-  }
-}
--- 3 EC2 ELB [test]/data.sh
+++
@@ -1,7 +0,0 @@
-touch test1.txt
-touch test2.txt
-sudo amazon-linux-extras install nginx1 -y
-touch test3.txt
-sudo systemctl start nginx
-touch test4.txt
-sudo systemctl enable nginx
