f84f1915c42c96068f2f870d1df7109a4f2b1075
Angelo Machils <angelo@machils.org>
2022-11-29 10:37:33 +0200

    Working Load Balancer, but no website visible when trying the DNS record for the lb.....

--- 3 EC2 ELB Task/modules/lb/main.tf
+++ 3 EC2 ELB Task/modules/lb/main.tf
@@ -77,6 +77,40 @@ module "auto-scaling-group-demo" {
   tags = local.tags
 }
 
+# Application Load Balancer
+resource "aws_lb_target_group" "lb_target" {
+  name        = "lb-target"
+  port        = 8089
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.vpc.vpc_id
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [module.asg_sg.security_group_id]
+  subnets            = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target.arn
+  }
+}
+
 # Supporting Resources
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
