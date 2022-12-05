db717b891ea1f0624469ef337c4b05fe3a7212a4
Angelo Machils <angelo@machils.org>
2022-12-05 15:22:49 +0200

    Got a working setup, albeit split up for now 	modified:   3 EC2 ELB Task/modules/network/main.tf 	new file:   3 EC2 ELB Task/modules/network/main.tf.old 	modified:   3 EC2 ELB Task/modules/network/vars.tf

--- 3 EC2 ELB Task/modules/network/main.tf
+++ 3 EC2 ELB Task/modules/network/main.tf
@@ -1,69 +1,123 @@
-# # Create Public Subnet
-resource "aws_subnet" "publicsubnet" {
-  vpc_id                  = var.vpc_id
-  cidr_block              = var.subnet_cidr[0]
-  availability_zone       = "eu-north-1a"
+locals {
+  public_subnets = {
+    "${var.region}a" = "10.10.101.0/24"
+    "${var.region}b" = "10.10.102.0/24"
+    "${var.region}c" = "10.10.103.0/24"
+  }
+  private_subnets = {
+    "${var.region}a" = "10.10.201.0/24"
+    "${var.region}b" = "10.10.202.0/24"
+    "${var.region}c" = "10.10.203.0/24"
+  }
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.10.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.service_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = "${aws_vpc.this.id}"
+
+  tags = {
+    Name = "${var.service_name}-internet-gateway"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count      = "${length(local.public_subnets)}"
+  cidr_block = "${element(values(local.public_subnets), count.index)}"
+  vpc_id     = "${aws_vpc.this.id}"
+
   map_public_ip_on_launch = true
+  availability_zone       = "${element(keys(local.public_subnets), count.index)}"
+
   tags = {
-    Name = var.subnet_name[0]
+    Name = "${var.service_name}-service-public"
   }
 }
 
-# Create Private Subnet
-resource "aws_subnet" "privatesubnet" {
-  vpc_id                  = var.vpc_id
-  cidr_block              = var.subnet_cidr[1]
-  availability_zone       = "eu-north-1b"
-  map_public_ip_on_launch = false
+resource "aws_subnet" "private" {
+  count      = "${length(local.private_subnets)}"
+  cidr_block = "${element(values(local.private_subnets), count.index)}"
+  vpc_id     = "${aws_vpc.this.id}"
+
+  map_public_ip_on_launch = true
+  availability_zone       = "${element(keys(local.private_subnets), count.index)}"
+
   tags = {
-    Name = var.subnet_name[1]
+    Name = "${var.service_name}-service-private"
   }
 }
 
-# Create Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = var.vpc_id
-}
+resource "aws_default_route_table" "public" {
+  default_route_table_id = "${aws_vpc.this.main_route_table_id}"
 
-# Create Route Table for Public Subnet
-resource "aws_route_table" "rt" {
-  vpc_id = var.vpc_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
   tags = {
-    Name = var.route_name[0]
+    Name = "${var.service_name}-public"
   }
 }
-resource "aws_route_table_association" "rt_associate_public" {
-  subnet_id      = aws_subnet.publicsubnet.id
-  route_table_id = aws_route_table.rt.id
+
+resource "aws_route" "public_internet_gateway" {
+  count                  = "${length(local.public_subnets)}"
+  route_table_id         = "${aws_default_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.this.id}"
+
+  timeouts {
+    create = "5m"
+  }
 }
 
-# Create EIP
-resource "aws_eip" "eip" {
+resource "aws_route_table_association" "public" {
+  count          = "${length(local.public_subnets)}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_default_route_table.public.id}"
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.this.id}"
+
+  tags = {
+    Name = "${var.service_name}-private"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = "${length(local.private_subnets)}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+
+resource "aws_eip" "nat" {
   vpc = true
-}
 
-# Create NAT Gateway
-resource "aws_nat_gateway" "gw" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.publicsubnet.id
-}
-
-# Create Route Table for NAT Gateway
-resource "aws_route_table" "rt_NAT" {
-  vpc_id = var.vpc_id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.gw.id
-  }
   tags = {
-    Name = var.route_name[1]
+    Name = "${var.service_name}-eip"
   }
 }
-resource "aws_route_table_association" "rt_associate_private" {
-  subnet_id      = aws_subnet.privatesubnet.id
-  route_table_id = aws_route_table.rt_NAT.id
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id     = "${aws_subnet.public.0.id}"
+
+  tags = {
+    Name = "${var.service_name}-nat-gw"
+  }
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = "${aws_route_table.private.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${aws_nat_gateway.this.id}"
+
+  timeouts {
+    create = "5m"
+  }
 }
--- 3 EC2 ELB Task/modules/network/vars.tf
+++ 3 EC2 ELB Task/modules/network/vars.tf
@@ -1,21 +1,11 @@
-# Define CIDR Block for subnet
-variable "subnet_cidr" {
-  type    = list(any)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+# Create the variable for the region name
+variable "region" {
+  type    = string
+  default = "eu-north-1"
 }
 
-# Define the name of the subnets
-variable "subnet_name" {
-  type    = list(any)
-  default = ["Public Subnet", "Private Subnet"]
-}
-
-variable "vpc_id" {
-  type = string
-}
-
-# Define the name of the route tables
-variable "route_name" {
-  type    = list(any)
-  default = ["Public Route", "Private Route"]
+# Create the variable for the general name
+variable "service_name" {
+  type    = string
+  default = "am-machine"
 }
